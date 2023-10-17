f9d501f83fb84411ae0d45805d3cd66b0e918924
CW <52095472+open-minded13@users.noreply.github.com>
2023-10-17 12:27:57 -0700

    new file:   Part 1/Easy 704. Binary Search.py 	modified:   README.md

---
+++ Part 1/Easy 704. Binary Search.py
@@ -0,0 +1,53 @@
+# Date of Last Practice: Oct 17, 2023
+#
+# Time Complexity: O(log N), where N is the number of elements in the nums array.
+#                            In each iteration of the while loop,
+#                            you effectively halve the search space by updating the left or right pointer.
+#                            This logarithmic behavior makes binary search an efficient algorithm
+#                            for searching in sorted arrays.
+# Space Complexity: O(1) because the space used by the variables left, right, and pivot is not dependent
+#                   on the size of the input array.
+
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            pivot = (left + right) // 2
+            if target == nums[pivot]:
+                return pivot
+            elif target > nums[pivot]:
+                left = pivot + 1
+            else:
+                right = pivot - 1
+        return -1
+
+
+# Test cases
+if __name__ == "__main__":
+    solution = Solution()
+
+    # Example test cases
+    nums = [-1, 0, 3, 5, 9, 12]
+    target = 9
+    result = solution.search(nums, target)
+    print(result)  # Output should be 4
+
+    nums = [-1, 0, 3, 5, 9, 12]
+    target = 2
+    result = solution.search(nums, target)
+    print(result)  # Output should be -1
+
+    # Additional test cases
+    nums = [1, 2, 3, 4, 5, 6, 7, 8]
+    target = 5
+    result = solution.search(nums, target)
+    print(result)  # Output should be 4
+
+    nums = [1, 2, 3, 4, 5, 6, 7, 8]
+    target = 10
+    result = solution.search(nums, target)
+    print(result)  # Output should be -1
--- README.md
+++ README.md
@@ -21,7 +21,7 @@ Goal: To achieve the review of the application of the following data structures 
 - [x] [Easy 226. Invert Binary Tree](https://leetcode.com/problems/invert-binary-tree/) (picked in Blind 75)
 - [x] [Easy 232. Implement Queue using Stacks](https://leetcode.com/problems/implement-queue-using-stacks/)
 - [x] [Easy 242. Valid Anagram](https://leetcode.com/problems/valid-anagram/)
-- [ ] [Easy 704. Binary Search](https://leetcode.com/problems/binary-search/)
+- [x] [Easy 704. Binary Search](https://leetcode.com/problems/binary-search/)
 - [ ] [Easy 733. Flood Fill](https://leetcode.com/problems/flood-fill/)
 - [ ] [Medium 235. Lowest Common Ancestor of a Binary Search Tree](https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/)
 
