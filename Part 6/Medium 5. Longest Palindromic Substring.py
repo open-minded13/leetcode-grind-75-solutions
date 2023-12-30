457077f18882bb76acc04bdc9f7bd3dfe07137f1
Chia-Wei <52095472+open-minded13@users.noreply.github.com>
2023-12-30 10:38:41 -0800

    renamed:    Part 2/East 409. Longest Palindrome.py -> Part 2/Easy 409. Longest Palindrome.py 	new file:   Part 6/Medium 5. Longest Palindromic Substring.py 	modified:   README.md

---
+++ Part 6/Medium 5. Longest Palindromic Substring.py
@@ -0,0 +1,38 @@
+# Date of Last Practice: Dec 30, 2023
+#
+# Time Complexity: O(N^2), where N is the length of the string.
+#                  First, we iterate each character in the string, which is O(N).
+#                  Second, for each character, we potentially expand in both directions.
+#                  In the worst case, this expansion can be N/2 times or O(N).
+#                  Therefore, O(N * N / 2) = O(N^2).
+#
+# Space Complexity: O(1), as we only use constant extra space.
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if not s:
+            return ""
+
+        def expand_around_center(left, right):
+            while left >= 0 and right <= len(s) - 1 and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return right - left - 1
+
+        start, end = 0, 0
+        for index in range(len(s)):
+            len_1 = expand_around_center(index, index)
+            len_2 = expand_around_center(index, index + 1)
+            max_len = max(len_1, len_2)
+            if max_len > (end - start):
+                start = index - (max_len - 1) // 2
+                end = index + max_len // 2
+
+        return s[start : end + 1]
+
+
+# Test cases
+sol = Solution()
+print(sol.longestPalindrome("babad"))  # Output: "bab" or "aba"
+print(sol.longestPalindrome("cbbd"))  # Output: "bb"
