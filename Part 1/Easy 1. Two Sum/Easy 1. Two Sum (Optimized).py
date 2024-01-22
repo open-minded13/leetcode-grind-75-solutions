495481b8d50c6efb70a49353ea17874d71d42da7
Chia-Wei <52095472+open-minded13@users.noreply.github.com>
2024-01-21 17:59:51 -0800

    modified:   Part 1/Easy 1. Two Sum/Easy 1. Two Sum (Optimized).py

--- Part 1/Easy 1. Two Sum/Easy 1. Two Sum (Optimized).py
+++ Part 1/Easy 1. Two Sum/Easy 1. Two Sum (Optimized).py
@@ -1,7 +1,12 @@
-# Time complexity: O(N);
-# Space Complexity: O(N);
-# Overall, the time complexity of this solution is O(n) since it only iterates over the input list once.
-# The space complexity is also O(n) since the dictionary mp can store up to all elements of the input vector.
+# Date of Last Practice: June 11, 2023 -> Jan 21, 2024
+#
+# Time complexity: O(N), where N is the length of nums.
+#                  Overall, the time complexity of this solution is O(n)
+#                  since it only iterates over the input list once.
+#
+# Space Complexity: O(N), where N is the length of nums.
+#                   The space complexity is also O(n) since
+#                   the dictionary mp can store up to all elements of the input vector.
 
 
 class Solution:
